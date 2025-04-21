f57929ea4be6c816654b258554b45fddb8e55d1f
Koen van Eijk <vaneijk.koen@gmail.com>
2025-04-22 00:59:36 +0200

    tinycoder:  Changes to samplev3.py

---
+++ samplev3.py
@@ -0,0 +1,8 @@
+import string
+
+def print_alphabet():
+    """Prints all lowercase letters of the English alphabet."""
+    print(string.ascii_lowercase)
+
+if __name__ == "__main__":
+    print_alphabet()
